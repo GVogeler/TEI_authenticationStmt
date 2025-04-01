493e75e50fab018198c8eefc3ff8b7ba5031eb6b
Ash Clark <amclark42@gmail.com>
2025-04-01 08:43:03 -0400

    Update and streamline popup JS through `showPopupFootnote()`.
    Use 'let' and 'const' keywords instead of 'var'. Use arrow
    functions, `addEventListener()`, and filtering on arrays.
    
    I cut out a function layer by triggering `showPopupFootnote()`
    directly from a link click. That function now takes an event object
    and uses it to determine the current event target and the footnote
    ID.

--- P5/webnav/popupFootnotes.js
+++ P5/webnav/popupFootnotes.js
@@ -5,66 +5,59 @@
   Created by Martin Holmes, 2013-07 and 2014-07, free for all for everything.
  */
  
- //Make these pointers global so we can use them everywhere.
- var footnotePopup = null;
- var footnotePopupContent = null;
- var biblFrame = null;
+ // Make these variables global so we can use them everywhere.
+ const footnotePopup = document.createElement('div');
+ const footnotePopupContent = document.createElement('div');
+ let biblFrame = null;
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  footnotePopup = document.createElement('div');
   footnotePopup.setAttribute('id', 'footnotePopup');
-  var closer = document.createElement('div');
-  closer.setAttribute('class', 'footnotePopupCloser');
-  closer.onclick = function() {this.parentNode.style.display = "none"};
-  var x = document.createTextNode('x');
-  closer.appendChild(x);
-  footnotePopupContent = document.createElement('div');
+  let closeBtn = document.createElement('div'); // This should really be a <button>
+  closeBtn.setAttribute('class', 'footnotePopupCloser');
+  closeBtn.addEventListener('click', e => { e.target.parentNode.style.display = "none" });
+  closeBtn.appendChild(document.createTextNode('x'));
   footnotePopupContent.setAttribute('id', 'footnotePopupContent');
-  footnotePopup.appendChild(closer);
+  footnotePopup.appendChild(closeBtn);
   footnotePopup.appendChild(footnotePopupContent);
   document.getElementsByTagName('body')[0].appendChild(footnotePopup);
 
 //Now we work through the links to footnotes.
-  var links = document.getElementsByTagName('a');
-  for (var i=0; i<links.length; i++){
-    if (links[i].hasAttribute("href") && links[i].getAttribute('href').substring(0, 5) == '#Note'){
-      if (links[i].getAttribute('class') != 'link_return'){
-        /* When an anchor link is clicked, we use its @href value to determine which 
-         note's content should be loaded into the popup. */
-        links[i].onclick = function (e) {
-          e.preventDefault();
-          /* The event's currentTarget is the element that has the event attached 
-           (<a>), rather than the descendant element that fired the event (<sup>). */
-          var anchor = e.currentTarget,
-              dest = anchor.getAttribute('href');
-          if ( dest !== null ) {
-            dest = dest.substring(1);
-            showPopupFootnote(anchor, dest);
-          } else {
-            console.warn('Could not generate note content for anchor:');
-            console.warn(e.currentTarget);
-          }
-        }
-      }
-    }
-  }
+  // Create an array of links to notes.
+  let links = Array.from(document.querySelectorAll('a[href ^= "#Note"]'))
+    // Filter out links at the bottom back to the content being notated.
+    .filter(link => (! link.classList.contains('link_return')));
+  // Add event listeners to show popup notes when each link is clicked.
+  links.forEach(link => { link.addEventListener('click', showPopupFootnote) });
+  
   addBiblFrame();
 });
 
-function showPopupFootnote(el, footnoteId){
-  var footnote = document.getElementById(footnoteId);
-  if ((footnotePopup == null)||(footnotePopupContent == null)||(footnote == null)){
+function showPopupFootnote(event){
+  event.preventDefault();
+  /* The event's currentTarget is the element that has the event attached 
+   (<a>), rather than the descendant element that fired the event (<sup>). */
+  let el = event.currentTarget,
+      footnoteId = el.getAttribute('href');
+  if ( footnoteId === null ) {
+    console.warn('Could not generate note content for anchor:');
+    console.warn(el);
+    return;
+  }
+  // Take the '#' off the link ID.
+  footnoteId = footnoteId.substring(1);
+  let footnote = document.getElementById(footnoteId);
 //If something is missing, we just default to original behaviour and jump to the footnote.
+  if ((footnotePopup === null)||(footnotePopupContent === null)||(footnote === null)){
     document.location.hash = footnoteId;
     return;
   }
 //Otherwise, we populate the popup with the content of the footnote, and show it.
-  var cloneFootnote = footnote.cloneNode(true);
+  let cloneFootnote = footnote.cloneNode(true);
 //We need to remove the id because it'll be a duplicate.
   cloneFootnote.setAttribute('id', '');
   // Also remove links back to the text because they are not needed in popups.
-  cloneFootnote.querySelectorAll("a.link_return").forEach(function(a) {a.parentElement.removeChild(a)});
+  cloneFootnote.querySelectorAll("a.link_return").forEach(a => {a.parentElement.removeChild(a)});
 //Add it to the popup.
   clearContent(footnotePopupContent);
   footnotePopupContent.appendChild(cloneFootnote);
